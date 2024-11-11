617028dd43f257cf2bb172dd2cf04fd0a8f086c3
Manvith <manvithshetty179@gmail.com>
2024-11-12 00:50:45 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,22 @@ import Image from "next/image";
 
 import styles from "./page.module.css";
 
+export async function generateMetadata(): Promise<Metadata> {
+  const frameTags = await getFrameMetadata(
+    `${process.env.VERCEL_URL || "http://localhost:3000"}/api`
+  );
+  const metadataObject: {
+    [name: string]: string | number | (string | number)[];
+  } = {};
+  frameTags.forEach(({ property, content }) => {
+    metadataObject[property] = content;
+  });
+
+  return {
+    ...metadataObject,
+  };
+}
+
 export default function Home() {
   return (
     <main className={styles.main}>
